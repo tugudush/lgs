3abdf5d39028bbb4096e46fe02504f89a04ae374
tugudush <jerome2kph@gmail.com>
2017-05-21 10:55:51 +0800

    fixed styles.inc.php

--- styles.inc.php
+++ styles.inc.php
@@ -4,11 +4,7 @@
 <link href="<?php echo $site_url; ?>/css/jquery.datetimepicker.css" rel="stylesheet">
 <link href="<?php echo $site_url; ?>/css/font-awesome.css" rel="stylesheet">
 <link href="<?php echo $site_url; ?>/css/fonts.css" rel="stylesheet">
-<<<<<<< HEAD
-<link href="<?php echo $site_url; ?>/css/presets.css?v=0.1.3" rel="stylesheet">
-=======
 <link href="<?php echo $site_url; ?>/css/presets.css?v=1.0" rel="stylesheet">
->>>>>>> dcbb4892746f7057b5421752d5996f33c1ecfaaf
 <link href="<?php echo $site_url; ?>/css/style.css?v=1.0" rel="stylesheet">
 <link href="<?php echo $site_url; ?>/css/media.css?v=1.0" rel="stylesheet">
 <link href="<?php echo $site_url; ?>/css/print.css?v=1.0" rel="stylesheet">
